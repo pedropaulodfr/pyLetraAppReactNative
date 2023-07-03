8c79304fefee5924c2cee27c5fb92b91885f7c21
pedropaulodfr <pedropaulodfr@outlook.com>
2023-07-03 13:55:25 -0300

    Função de tradução

--- App.js
+++ App.js
@@ -2,6 +2,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import Logo from './src/components/Logo';
 import Form from './src/components/Form';
 
+
 export default function App() {
   return (
     <View style={styles.container}>
--- src/components/LetraMusica/index.js
+++ src/components/LetraMusica/index.js
@@ -1,39 +1,42 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, Text, TextInput, Image, StyleSheet, ScrollView, Button, Share, Pressable } from "react-native"
 import styles from './style'
-import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import IconTranslate from 'react-native-vector-icons/MaterialCommunityIcons';
 import IconShare from 'react-native-vector-icons/AntDesign';
 
 
 export default function LetraMusica(props) {
     let lyric = props.letra;
+    const[traducao, setTraducao] = useState(false)
+    const[traducaoColor, setTraducaoColor] = useState("#A9A9A9")
 
-    // Verificar tradução
-    //console.log(lyric.mus[0].translate[0].text);
-
+    // Compartilhamento
     const onShare = async () => {
         const result = await Share.share({
             message:
             lyric != '' && `${lyric.mus[0].name}\n${lyric.art.name}\n\n${lyric.mus[0].text}\n\nDisponível em: ${lyric.mus[0].url}`
         })
-
-        if (result.action === Share.sharedAction) {
-            if (result.activityType) {
-              // shared with activity type of result.activityType
-            } else {
-              // shared
-            }
-          } else if (result.action === Share.dismissedAction) {
-            // dismissed
-          }
+    }
+    
+    // Tradução
+    const onTranslate = () => {
+        if (traducao) {
+            setTraducao(false)
+            setTraducaoColor("#A9A9A9")
+        } else {
+            setTraducao(true)
+            setTraducaoColor("#1992EA")
+        }
     }
 
     return(
         <View style={styles.formLyricTextContainer}>
             <ScrollView >
+                {/* Imagem do artista */}
                 <View style={{width: '100%', alignItems: 'center'}}>
                     <Image style={styles.imageArtist} source={{uri: props.imagemArtista}} />
                 </View>
+                {/* Titulo da música e nome do artista */}
                 <View style={{width: '100%', alignItems: 'center', marginTop: 10}}>
                     <Text style={styles.titleMusic}>{lyric != '' && `${lyric.mus[0].name}`}</Text>
                     <Text style={styles.nameArtist}>{lyric != '' && `${lyric.art.name}`}</Text>
@@ -41,25 +44,28 @@ export default function LetraMusica(props) {
 
                 {/* Botão de tradução */}
                 <View style={{width: '100%', alignItems: 'center'}}>
-                    {false && 
-                        <Icon 
-                            name="translate" 
-                            size={25} 
-                            color="#A9A9A9" 
-                            style={{top: 20, right: 30, position: 'absolute'}} 
-                            onPress={() => {}}
-                        /> 
-                    }
+                    <IconTranslate
+                        name="translate" 
+                        size={25} 
+                        color={traducaoColor}
+                        style={{top: 20, right: 70, position: 'absolute', zIndex: 30}} 
+                        onPress={onTranslate}
+                    /> 
 
-                        <IconShare
-                            name="sharealt" 
-                            size={25} 
-                            color="#A9A9A9" 
-                            style={{top: 20, right: 30, position: 'absolute', zIndex: 30}} 
-                            onPress={onShare}
-                        /> 
+                    <IconShare
+                        name="sharealt" 
+                        size={25} 
+                        color="#A9A9A9" 
+                        style={{top: 20, right: 30, position: 'absolute', zIndex: 30}} 
+                        onPress={onShare}
+                    /> 
                     
-                    <Text style={styles.formLyricText}>{lyric != '' && `${lyric.mus[0].text}\n\nDisponível em: ${lyric.mus[0].url}\n\n\n`}</Text>
+                    <Text style={styles.formLyricText}>
+                        {
+                            (lyric != '' && !traducao) 
+                            ? `${lyric.mus[0].text}\n\nDisponível em: ${lyric.mus[0].url}\n\n\n` 
+                            : `${lyric.mus[0].translate[0].text}\n\nDisponível em: ${lyric.mus[0].url}\n\n\n`
+                        }</Text>
                 </View>
             </ScrollView>
         </View>
